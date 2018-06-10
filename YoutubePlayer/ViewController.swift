1a1af9c57cb93a19d4245ee91ebda95df5e0a545
Leena Erhayel <leenaerhayel@gmail.com>
2018-06-09 23:37:02 -0700

    Initial Commit

---
+++ YoutubePlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YoutubePlayer
+//
+//  Created by Hackeru_Student on 6/9/18.
+//  Copyright © 2018 Leena Erhayel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
